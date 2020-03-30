7cb9afce5da1aa85d794591c65cef86569a7700a
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-03-30 17:48:11 +0200

    Test case where rejectWithValue is used to reject the async thunk

--- src/__tests__/createAsyncReducer.spec.ts
+++ src/__tests__/createAsyncReducer.spec.ts
@@ -1,124 +1,162 @@
 import createAsyncReducer from '../createAsyncReducer';
 import {
-  createAsyncThunk,
-  configureStore,
-  SerializedError,
+    createAsyncThunk,
+    configureStore,
+    SerializedError,
 } from '@reduxjs/toolkit';
 
 function setUp() {
-  const asyncActions = createAsyncThunk('test', async (id: string) => {
-    if (id.startsWith('fail-')) {
-      throw new Error('Failed ' + id);
-    }
-    return { id };
-  });
-  const reducer = createAsyncReducer(
-    null as { id: string } | null,
-    asyncActions
-  );
-
-  const store = configureStore({ reducer });
-
-  return { asyncActions, reducer, store };
-}
-
-describe('createAsyncReducer', () => {
-  it('Is in the initial state', () => {
-    const { store } = setUp();
-
-    expect(store.getState()).toEqual({
-      data: null,
-      isLoading: false,
-      requestError: null,
-      requestId: null,
+    const asyncActions = createAsyncThunk<
+        { id: string },
+        string,
+        { rejectValue: { error: string } }
+    >('test', async (id, { rejectWithValue }) => {
+        if (id.startsWith('fail-')) {
+            throw new Error('Failed ' + id);
+        } else if (id.startsWith('reject-')) {
+            return rejectWithValue({ error: 'rejected: ' + id });
+        }
+        return { id };
     });
-  });
-
-  it('Transitions to loading state when an action is dispatched', async () => {
-    const { store, asyncActions } = setUp();
-
-    const dispatchPromise = store.dispatch(asyncActions('some-id'));
-
-    const { requestId, ...state } = store.getState();
-
-    expect(state).toEqual({
-      data: undefined,
-      isLoading: true,
-      requestError: null,
-    });
-
-    await dispatchPromise.then(result =>
-      expect(result.meta.requestId).toEqual(requestId)
-    );
-
-    expect(store.getState()).toEqual({
-      data: { id: 'some-id' },
-      isLoading: false,
-      requestError: null,
-      requestId: null,
-    });
-  });
-
-  it('Transitions to error state when an action fails', async () => {
-    const { store, asyncActions } = setUp();
-
-    const dispatchPromise = store.dispatch(asyncActions('fail-some-id'));
-
-    const { requestId, ...state } = store.getState();
-
-    expect(state).toEqual({
-      data: undefined,
-      isLoading: true,
-      requestError: null,
-    });
-
-    await dispatchPromise.then(result =>
-      expect(result.meta.requestId).toEqual(requestId)
-    );
-
-    const { requestError, ...newState } = store.getState();
-
-    expect(newState).toEqual({
-      data: undefined,
-      isLoading: false,
-      requestId: null,
-    });
-
-    expect((requestError as SerializedError).message).toEqual(
-      'Failed fail-some-id'
-    );
-  });
-
-  it('Can be created with a custom reducer', async () => {
-    const { asyncActions } = setUp();
-
-    const reducer = createAsyncReducer(
-      null as string | null,
-      asyncActions,
-      (_state, action) => action.payload.id
-    );
+    const reducer = createAsyncReducer<
+        { id: string },
+        string,
+        { rejectValue: { error: string } }
+    >(undefined, asyncActions);
 
     const store = configureStore({ reducer });
 
-    const dispatchPromise = store.dispatch(asyncActions('some-id'));
+    return { asyncActions, reducer, store };
+}
 
-    const { requestId, ...state } = store.getState();
+describe('createAsyncReducer', () => {
+    it('Is in the initial state', () => {
+        const { store } = setUp();
 
-    expect(state).toEqual({
-      data: undefined,
-      isLoading: true,
-      requestError: null,
+        expect(store.getState()).toEqual({
+            data: undefined,
+            isLoading: false,
+            requestError: null,
+            requestId: null,
+        });
     });
 
-    await dispatchPromise.then(result =>
-      expect(result.meta.requestId).toEqual(requestId)
-    );
+    it('Transitions to loading state when an action is dispatched', async () => {
+        const { store, asyncActions } = setUp();
 
-    expect(store.getState()).toEqual({
-      data: 'some-id',
-      isLoading: false,
-      requestError: null,
-      requestId: null,
+        const dispatchPromise = store.dispatch(asyncActions('some-id'));
+
+        const { requestId, ...state } = store.getState();
+
+        expect(state).toEqual({
+            data: undefined,
+            isLoading: true,
+            requestError: null,
+        });
+
+        await dispatchPromise.then(result =>
+            expect(result.meta.requestId).toEqual(requestId)
+        );
+
+        expect(store.getState()).toEqual({
+            data: { id: 'some-id' },
+            isLoading: false,
+            requestError: null,
+            requestId: null,
+        });
     });
-  });
+
+    it('Transitions to error state when an action fails', async () => {
+        const { store, asyncActions } = setUp();
+
+        const dispatchPromise = store.dispatch(asyncActions('fail-some-id'));
+
+        const { requestId, ...state } = store.getState();
+
+        expect(state).toEqual({
+            data: undefined,
+            isLoading: true,
+            requestError: null,
+        });
+
+        await dispatchPromise.then(result =>
+            expect(result.meta.requestId).toEqual(requestId)
+        );
+
+        const { requestError, ...newState } = store.getState();
+
+        expect(newState).toEqual({
+            data: undefined,
+            isLoading: false,
+            requestId: null,
+        });
+
+        expect((requestError as SerializedError).message).toEqual(
+            'Failed fail-some-id'
+        );
+    });
+
+    it('Transitions to error state when an action is rejected', async () => {
+        const { store, asyncActions } = setUp();
+
+        const dispatchPromise = store.dispatch(asyncActions('reject-some-id'));
+
+        const { requestId, ...state } = store.getState();
+
+        expect(state).toEqual({
+            data: undefined,
+            isLoading: true,
+            requestError: null,
+        });
+
+        await dispatchPromise.then(result =>
+            expect(result.meta.requestId).toEqual(requestId)
+        );
+
+        const { requestError, ...newState } = store.getState();
+
+        expect(newState).toEqual({
+            data: undefined,
+            isLoading: false,
+            requestId: null,
+        });
+
+        expect(requestError as { error: string }).toEqual({
+            error: 'rejected: reject-some-id',
+        });
+    });
+
+    it('Can be created with a custom reducer', async () => {
+        const { asyncActions } = setUp();
+
+        const reducer = createAsyncReducer<
+            string,
+            { id: string },
+            string,
+            { rejectValue: { error: string } }
+        >(undefined, asyncActions, (_state, action) => action.payload.id);
+
+        const store = configureStore({ reducer });
+
+        const dispatchPromise = store.dispatch(asyncActions('some-id'));
+
+        const { requestId, ...state } = store.getState();
+
+        expect(state).toEqual({
+            data: undefined,
+            isLoading: true,
+            requestError: null,
+        });
+
+        await dispatchPromise.then(result =>
+            expect(result.meta.requestId).toEqual(requestId)
+        );
+
+        expect(store.getState()).toEqual({
+            data: 'some-id',
+            isLoading: false,
+            requestError: null,
+            requestId: null,
+        });
+    });
 });
--- src/requestId.ts
+++ src/requestId.ts
@@ -1,139 +1,139 @@
 import {
-  SerializedError,
-  Reducer,
-  PayloadAction,
-  Draft,
-  Dispatch,
+    SerializedError,
+    Reducer,
+    PayloadAction,
+    Draft,
+    Dispatch,
 } from '@reduxjs/toolkit';
 import invariant from 'tiny-invariant';
 
 export type WithRequestIdState<S, ThunkApiConfig> =
-  | WithRequestIdStateLoading
-  | WithRequestIdStateSuccess<S>
-  | WithRequestIdStateFailure<ThunkApiConfig>;
+    | WithRequestIdStateLoading
+    | WithRequestIdStateSuccess<S>
+    | WithRequestIdStateFailure<ThunkApiConfig>;
 
 type DraftReducer<S, A> = (state: S | Draft<S>, action: A) => S | void;
 
 interface WithRequestIdStateLoading {
-  data: undefined;
-  isLoading: true;
-  requestId: string;
-  requestError: null;
+    data: undefined;
+    isLoading: true;
+    requestId: string;
+    requestError: null;
 }
 
 interface WithRequestIdStateSuccess<S> {
-  data: S;
-  isLoading: false;
-  requestId: null;
-  requestError: null;
+    data: S;
+    isLoading: false;
+    requestId: null;
+    requestError: null;
 }
 
 interface WithRequestIdStateFailure<ThunkApiConfig extends AsyncThunkConfig> {
-  data: undefined;
-  isLoading: false;
-  requestId: null;
-  requestError: GetRejectValue<ThunkApiConfig> | SerializedError;
+    data: undefined;
+    isLoading: false;
+    requestId: null;
+    requestError: GetRejectValue<ThunkApiConfig> | SerializedError;
 }
 
 type GetRejectValue<ThunkApiConfig> = ThunkApiConfig extends {
-  rejectValue: infer RejectValue;
+    rejectValue: infer RejectValue;
 }
-  ? RejectValue
-  : unknown;
+    ? RejectValue
+    : unknown;
 
 type AsyncThunkConfig = {
-  state?: unknown;
-  dispatch?: Dispatch;
-  extra?: unknown;
-  rejectValue?: unknown;
+    state?: unknown;
+    dispatch?: Dispatch;
+    extra?: unknown;
+    rejectValue?: unknown;
 };
 
 export function withRequestId<S, ThunkApiConfig extends AsyncThunkConfig = {}>(
-  initialState: S
+    initialState: S
 ): WithRequestIdState<S, ThunkApiConfig> {
-  return {
-    data: initialState,
-    isLoading: false,
-    requestId: null,
-    requestError: null,
-  };
+    return {
+        data: initialState,
+        isLoading: false,
+        requestId: null,
+        requestError: null,
+    };
 }
 
 export function storeRequestId<
-  S,
-  Payload = void,
-  ThunkArg = unknown,
-  A extends PayloadAction<
-    Payload,
-    string,
-    { arg: ThunkArg; requestId: string }
-  > = PayloadAction<Payload, string, { arg: ThunkArg; requestId: string }>,
-  E = SerializedError
+    S,
+    Payload = void,
+    ThunkArg = unknown,
+    A extends PayloadAction<
+        Payload,
+        string,
+        { arg: ThunkArg; requestId: string }
+    > = PayloadAction<Payload, string, { arg: ThunkArg; requestId: string }>,
+    E = SerializedError
 >(): DraftReducer<WithRequestIdState<S, E>, A> {
-  return (_state, action) => {
-    return {
-      isLoading: true,
-      data: undefined,
-      requestError: null,
-      requestId: action.meta.requestId,
+    return (_state, action) => {
+        return {
+            isLoading: true,
+            data: undefined,
+            requestError: null,
+            requestId: action.meta.requestId,
+        };
     };
-  };
 }
 
 export function validateRequestIdSuccess<
-  S,
-  Payload,
-  ThunkArg = unknown,
-  Error = SerializedError,
-  A extends PayloadAction<
+    S,
     Payload,
-    string,
-    { arg: ThunkArg; requestId: string }
-  > = PayloadAction<Payload, string, { arg: ThunkArg; requestId: string }>
+    ThunkArg = unknown,
+    Error = SerializedError,
+    A extends PayloadAction<
+        Payload,
+        string,
+        { arg: ThunkArg; requestId: string }
+    > = PayloadAction<Payload, string, { arg: ThunkArg; requestId: string }>
 >(reducer: Reducer<S, A>): DraftReducer<WithRequestIdState<S, Error>, A> {
-  return (state, action) => {
-    if (state?.requestId === action.meta.requestId) {
-      return {
-        isLoading: false,
-        data: reducer(state?.data, action),
-        requestId: null,
-        requestError: null,
-      };
-    }
-    invariant(state !== undefined, 'state can not be undefined');
-    return state as any;
-  };
+    return (state, action) => {
+        if (state?.requestId === action.meta.requestId) {
+            return {
+                isLoading: false,
+                data: reducer(state?.data, action),
+                requestId: null,
+                requestError: null,
+            };
+        }
+        invariant(state !== undefined, 'state can not be undefined');
+        return state as any;
+    };
 }
 
 export function validateRequestIdFailure<
-  S,
-  ThunkArg,
-  ThunkApiConfig extends AsyncThunkConfig = {},
-  Error extends SerializedError | GetRejectValue<ThunkApiConfig> =
-    | SerializedError
-    | GetRejectValue<ThunkApiConfig>,
-  A extends PayloadAction<
-    Error | undefined,
-    string,
-    { arg: ThunkArg; requestId: string; aborted: boolean },
-    SerializedError
-  > = PayloadAction<
-    Error | undefined,
-    string,
-    { arg: ThunkArg; requestId: string; aborted: boolean },
-    SerializedError
-  >
+    S,
+    ThunkArg,
+    ThunkApiConfig extends AsyncThunkConfig = {},
+    Error extends SerializedError | GetRejectValue<ThunkApiConfig> =
+        | SerializedError
+        | GetRejectValue<ThunkApiConfig>,
+    A extends PayloadAction<
+        Error | undefined,
+        string,
+        { arg: ThunkArg; requestId: string; aborted: boolean },
+        SerializedError
+    > = PayloadAction<
+        Error | undefined,
+        string,
+        { arg: ThunkArg; requestId: string; aborted: boolean },
+        SerializedError
+    >
 >(): DraftReducer<WithRequestIdState<S, Error>, A> {
-  return (state, action) => {
-    if (state?.requestId === action.meta.requestId) {
-      return {
-        isLoading: false,
-        data: undefined,
-        requestId: null,
-        requestError: action.error || action.payload,
-      };
-    }
-    invariant(state !== undefined, 'state can not be undefined');
-    return state as any;
-  };
+    return (state, action) => {
+        if (state?.requestId === action.meta.requestId) {
+            return {
+                isLoading: false,
+                data: undefined,
+                requestId: null,
+                requestError: action.payload || action.error,
+            };
+        }
+        invariant(state !== undefined, 'state can not be undefined');
+        return state as any;
+    };
 }
